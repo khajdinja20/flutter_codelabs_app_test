ee36014f9b81bfb087742f86b5a03e7ac0d3daa7
AtriaGaming <khajdinja20@foi.hr>
2023-10-27 12:20:11 +0200

    #61 Update color

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         ),
         home: MyHomePage(),
       ),
